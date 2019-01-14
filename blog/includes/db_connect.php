c823dc0b5d962fc4ca5c3168219081ce1a1fbd9e
julietta816 <julietta816@o2.pl>
2019-01-14 19:41:22 +0100

    serwer

---
+++ blog/includes/db_connect.php
@@ -0,0 +1,6 @@
+<?php
+    $host="sql1.5v.pl";
+    $db_user="db-user45080";
+    $db_pass="22ndv9cytgaa0yro";
+    $db_name="db-user45080";
+?>
---
+++ blog/register_success.php
@@ -0,0 +1,47 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['register_success']))
+    {
+        header('Location: index.php');
+        exit();
+    }
+    else
+    {
+        unset($_SESSION['register_success']);
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Blog o podróżowaniu - Udana rejestracja</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <script src='https://www.google.com/recaptcha/api.js'></script>
+</head>
+<body>
+    <?php 
+		require_once('/var/www/vhosts/letthejourneybegin.5v.pl/httpdocs/look/title.php');
+    ?>
+
+    <?php 
+		require_once('/var/www/vhosts/letthejourneybegin.5v.pl/httpdocs/look/menu.php');
+	?>
+
+<div id="glowna">
+    <div class="post">
+    <div style='margin-left:30px;'><h2>Dziękujemy za rejestrację! Zaloguj się na konto!</h2> </div>
+    </div>
+</div>
+<?php
+	require_once('/var/www/vhosts/letthejourneybegin.5v.pl/httpdocs/look/sidebar.php');
+?>
+
+<?php
+	require_once('/var/www/vhosts/letthejourneybegin.5v.pl/httpdocs/look/footer.php');
+?>
+    
+</body>
+</html>
